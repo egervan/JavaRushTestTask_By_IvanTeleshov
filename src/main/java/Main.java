6ff6457b5bf5d68eaa2be627d8a1e324576f46b0
egervan <ivant66@yandex.ru>
2016-05-18 12:37:19 +0300

    Связка Hibernate с Spring завершена.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,12 +11,20 @@ public class Main {
                 new String[] { "applicationContext.xml" }, true);*/
         FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/applicationContext.xml");
         UserDao userDao = (UserDao) context.getBean("userDao");
-        User data1 = new User("Alex", 20, false);
+
+        User user = userDao.getUserById(6);
+        user.setAge(48);
+        userDao.updateUser(user);
+                /*while(userDao.getAllUsers().size() > 7)
+        {
+            userDao.deleteUser(userDao.getAllUsers().size()-1);
+        }*/
+      /*  User data1 = new User("Alex", 20, false);
         userDao.addUser(data1);
         User data2 = new User("Bob", 21, false);
         userDao.addUser(data2);
         User data3 = new User("Alice", 19, false);
-        userDao.addUser(data3);
+        userDao.addUser(data3);*/
         System.out.println(userDao.getAllUsers().size());
     }
 }
